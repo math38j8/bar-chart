9f488993b1d528cf3fc16bf2e3a3c8637637cf40
MathildeSahlholdt <math38j8@stud.kea.dk>
2021-09-04 13:22:38 +0200

    comments on all the code

--- script.js
+++ script.js
@@ -10,37 +10,37 @@ let arrLength = 0;
 loop();
 
 function loop() {
-  randomNumber = Math.floor(Math.random() * 32);
-  arrLength++;
-  if (arrLength < 40) {
-    valuesArr.push(randomNumber);
+  randomNumber = Math.floor(Math.random() * 32);  //making a random number
+  arrLength++;                                    //arrLength should add 1 everytime it loops
+  if (arrLength < 40) {                           //if arrLength is smaller than 40
+    valuesArr.push(randomNumber);                 //adds random number in the end of the array
     console.log(valuesArr);
-    setTimeout(loop, 1000);
-    displayBars();
-  } else {
-    valuesArr.push(randomNumber);
-    valuesArr.shift();
+    setTimeout(loop, 1000);                       //after 1000 ms call loop function again
+    displayBars();                               
+  } else {                                        //if arrLength is not smaller than 40 then...
+    valuesArr.push(randomNumber);                 //adds random number in the end of the array
+    valuesArr.shift();                            //removes the first content in the array
     console.log(valuesArr);
-    setTimeout(loop, 1000);
+    setTimeout(loop, 1000);                       //after 1000 ms call loop function again
     displayBars();
   }
 }
 
 function displayBars() {
   if (arrLength < 40) {
-    newDiv = document.createElement("div");
-    document.querySelector("#chart").appendChild(newDiv);
+    newDiv = document.createElement("div");                     //creates element div
+    document.querySelector("#chart").appendChild(newDiv);       //append the new div in the #chart
     addClass();
   } else {
-    document.querySelector("#chart").firstChild.remove();
-    newDiv = document.createElement("div");
-    document.querySelector("#chart").appendChild(newDiv);
+    document.querySelector("#chart").firstChild.remove();       //removes the first child of the #chart
+    newDiv = document.createElement("div");                     //creates element div
+    document.querySelector("#chart").appendChild(newDiv);       //append the new div in the #chart
     addClass();
   }
 }
 
 function addClass() {
-  newDiv.setAttribute("class", "bar");
-  newDiv.style.height = `${randomNumber / 2}vw`;
-  newDiv.style.opacity = `${randomNumber * 3}%`;
-}
+  newDiv.setAttribute("class", "bar");                          //set attribute class and bar on the new div
+  newDiv.style.height = `${randomNumber / 2}vw`;                //styles the height of the new divs with a random number
+  newDiv.style.opacity = `${randomNumber * 3}%`;                //styles the opacity of the new divs with a random number
+}  
